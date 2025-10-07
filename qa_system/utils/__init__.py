eeda395f2dce2b4ebc359c17e070c1ee2c7d63b7
Hamad <hamadir22@gmail.com>
2025-10-07 13:04:22 +0100

    use config variables instead of static strings

--- qa_system/utils/__init__.py
+++ qa_system/utils/__init__.py
@@ -1,4 +1,4 @@
-from .config import Settings, get_settings
+from .config import Settings
 
-__all__ = ["Settings", "get_settings"]
+__all__ = ["Settings"]
 
--- qa_system/utils/config.py
+++ qa_system/utils/config.py
@@ -4,4 +4,7 @@ from pydantic import BaseModel
 class Settings(BaseModel):
     retrieval_top_k: int = 20
     rerank_top_k: int = 5
+    model_name: str = "lightonai/Reason-ModernColBERT"
+    index_folder: str = "index"
+    index_name: str = "hotpotqa-colbert-index"
 
