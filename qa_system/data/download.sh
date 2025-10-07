fa130a031991db8f7a574ffe1b124f383ecc5a22
Hamad <hamadir22@gmail.com>
2025-10-07 12:47:51 +0100

    dataset EDA

---
+++ qa_system/data/download.sh
@@ -0,0 +1,7 @@
+# go to the data directory
+# chmod +x download.sh
+# then run ./download.sh 
+
+wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_distractor_v1.json
+wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_fullwiki_v1.json
+wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_train_v1.1.json
