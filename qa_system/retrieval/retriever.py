6e69ff134c540dac0d2057e7934ca01f108e6989
Hamad <hamadir22@gmail.com>
2025-10-13 16:26:20 +0300

    retriever logic

--- qa_system/retrieval/retriever.py
+++ qa_system/retrieval/retriever.py
@@ -3,9 +3,18 @@ from typing import Dict, List
 
 class Retriever:
     def __init__(self) -> None:
+
+        # initialize the index 
+        # initialize the retriever
+        # initialize the document_ids_to_sentence (from the json file)
         pass
 
     def retrieve(self, query: str, top_k: int = 20) -> List[Dict]:
-        
+
+        # 1. encoder the query (generate embedding)
+        # 2. retrieve top k documents
+        # 3. convert the document ids to sentences (from the json file)
+        # 4. return the documents (in text)
+
         return [{'text': 'text'}]
 
