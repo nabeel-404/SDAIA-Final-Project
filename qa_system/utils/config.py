891d773dd2628ae3b5f02c33978edf6c00aa1c42
Hamad <hamadir22@gmail.com>
2025-10-16 18:23:18 +0300

    index path small fix

--- qa_system/utils/config.py
+++ qa_system/utils/config.py
@@ -1,10 +1,18 @@
 from pydantic import BaseModel
+import os
 
 
 class Settings(BaseModel):
     retrieval_top_k: int = 20
     rerank_top_k: int = 5
     model_name: str = "lightonai/Reason-ModernColBERT"
-    index_folder: str = "qa_system/retrieval/index"
+
+    # index folder should be a full path relative to repo root
+    index_folder: str = os.path.join(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")),
+        "qa_system",
+        "retrieval",
+        "index",
+    )
     index_name: str = "hotpotqa-colbert-index"
 
